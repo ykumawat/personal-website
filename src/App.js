9c61f50c4330b2567cec9278a2b0af4e05577480
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-11-14 17:47:02 -0500

    fixed padding on social media links

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ class App extends Component {
       <div class="container">
         <div class="row">
           <div class="col-2">
-            <div>
+            <div style={{paddingTop:"15px"}}>
               <a href='https://www.linkedin.com/in/ykumawat/'><img class="social-media-links" src='https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_color-512.png'/></a>
             </div>
-            <div>
+            <div style={{paddingTop:"15px"}}>
               <a href='https://github.com/ykumawat'><img class="social-media-links" src='https://image.flaticon.com/icons/svg/25/25231.svg'/></a>
             </div>
-            <div>
+            <div style={{paddingTop:"15px"}}>
               <a href='https://twitter.com/yamini011'><img class="social-media-links" src='https://cdn3.iconfinder.com/data/icons/free-social-icons/67/twitter_circle_color-256.png'/></a>
             </div>
           </div>
@@ -31,6 +31,7 @@ class App extends Component {
         </div>
       </div>
       <div>
+
         projects go here projects go here projects go here projects go here projects go here projects go here projects go here projects go here
       </div>
     </div>
