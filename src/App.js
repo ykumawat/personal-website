3cfbe79cb933bcffe039054c4bbe27c802a7342a
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-11-14 17:36:27 -0500

    adding header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-2">
+            <div>
+              <a href='https://www.linkedin.com/in/ykumawat/'><img class="social-media-links" src='https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_color-512.png'/></a>
+            </div>
+            <div>
+              <a href='https://github.com/ykumawat'><img class="social-media-links" src='https://image.flaticon.com/icons/svg/25/25231.svg'/></a>
+            </div>
+            <div>
+              <a href='https://twitter.com/yamini011'><img class="social-media-links" src='https://cdn3.iconfinder.com/data/icons/free-social-icons/67/twitter_circle_color-256.png'/></a>
+            </div>
+          </div>
+        <div class="col-3 name">
+            Yamini
+        </div>
+        <div class="col-2">
+          <img class="profile-image" src='https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/1/005/0ad/090/0b38c4b.jpg'/>
+        </div>
+        <div class="col-3 name">
+            Kumawat
+        </div>
+        <div class="col-2">
+          contact link here
+        </div>
       </div>
+      <div>
+        projects go here projects go here projects go here projects go here projects go here projects go here projects go here projects go here
+      </div>
+    </div>
     );
   }
 }
 
 export default App;
+
+// <img src='https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/1/005/0ad/090/0b38c4b.jpg' className="App-logo" alt="Yamini Kumawat" />
+// <h1>Yamini Kumawat</h1>
