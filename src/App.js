86cc28a91477afd62ff37377134be27aa5ce6ca1
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-11-18 22:02:24 -0500

    projects updated

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
         </div>
       </div>
       <div>
-        <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
+        <Tabs defaultActiveKey={2} animation={false} id="noanim-tab-example">
           <Tab eventKey={1} title="About Me"><About/></Tab>
           <Tab eventKey={2} title="Technical Projects"><Projects/></Tab>
           <Tab eventKey={3} title="Blog"><Blog/></Tab>
--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import { Carousel } from 'react-bootstrap'
 
 class Projects extends React.Component {
 
   render() {
     return (
-      <div>
-      hi
+      <div style={{paddingTop:"20px"}}>
+        <Carousel>
+          <Carousel.Item>
+            <div className="slide-container">
+            </div>
+          <Carousel.Caption>
+            <h3>S+S</h3>
+            <p>Easily and efficiently manage contact cards and notes with S+S, an image-to-text contact and document creator.</p>
+          </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+          <div className="slide-container">
+          </div>
+          <Carousel.Caption>
+            <h3>NYC Rec Ref</h3>
+            <p>Discover NYC recreational areas and favorite them on your in-app user profile for easy accessibility.</p>
+          </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+          <div className="slide-container">
+          </div>
+          <Carousel.Caption>
+            <h3>YaKush.ly</h3>
+            <p>Replicated a stock market and money sharing app using user created content.</p>
+          </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+          <div className="slide-container">
+          </div>
+          <Carousel.Caption>
+            <h3>What's Cookin'?</h3>
+            <p>Quickly find the perfect recipe using a specific preference and ingredient.</p>
+          </Carousel.Caption>
+          </Carousel.Item>
+
+        </Carousel>
       </div>
     )
   }
