3509f5f62911d345a022733eebc137cc7eaa444a
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-11-22 02:50:25 -0500

    links for repos

--- src/Projects.js
+++ src/Projects.js
@@ -10,7 +10,15 @@ class Projects extends React.Component {
           <Carousel.Item>
             <div className="slide-container">
               <button className="project-demo-button-link">Demo</button>
-              <button className="project-code-link">Code</button>
+              <a href="https://github.com/ykumawat/ss_final_project" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
+              <div>
+                <div className="project-video">
+                  coming soon
+                </div>
+                <div className="project-code-snippet">
+
+                </div>
+              </div>
             </div>
           <Carousel.Caption style={{color: "#91736C", textShadow: "none", paddingBottom: "10px"}}>
             <h3>S+S</h3>
@@ -21,7 +29,21 @@ class Projects extends React.Component {
           <Carousel.Item>
           <div className="slide-container">
             <button className="project-demo-button-link">Demo</button>
-            <button className="project-code-link">Code</button>
+            <div className="dropdown">
+              <button className="project-code-link">Code</button>
+              <div class="dropdown-content">
+                <a href="https://github.com/ykumawat/nyc-rec-frontend" target="_blank" rel="noopener noreferrer">Frontend</a>
+                <a href="https://github.com/ykumawat/nyc-rec-backend" target="_blank" rel="noopener noreferrer">Backend</a>
+              </div>
+            </div>
+            <div>
+            <div className="project-video">
+              coming soon
+            </div>
+            <div className="project-code-snippet">
+
+            </div>
+            </div>
           </div>
           <Carousel.Caption style={{color: "#91736C", textShadow: "none", paddingBottom: "10px"}}>
             <h3>NYC Rec Ref</h3>
@@ -32,7 +54,15 @@ class Projects extends React.Component {
           <Carousel.Item>
           <div className="slide-container">
             <button className="project-demo-button-link">Demo</button>
-            <button className="project-code-link">Code</button>
+            <a href="https://github.com/ykumawat/fake-stockmarket" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
+            <div>
+              <div className="project-video">
+
+              </div>
+              <div className="project-code-snippet">
+
+              </div>
+            </div>
           </div>
           <Carousel.Caption style={{color: "#91736C", textShadow: "none", paddingBottom: "10px"}}>
             <h3>YaKush.ly</h3>
@@ -43,7 +73,15 @@ class Projects extends React.Component {
           <Carousel.Item>
           <div className="slide-container">
             <button className="project-demo-button-link">Demo</button>
-            <button className="project-code-link">Code</button>
+            <a href="https://github.com/ykumawat/module-one-final-project-guidelines-web-071717" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
+            <div>
+            <div className="project-video">
+
+            </div>
+            <div className="project-code-snippet">
+
+            </div>
+            </div>
           </div>
           <Carousel.Caption style={{color: "#91736C", textShadow: "none", paddingBottom: "10px"}}>
             <h3>What's Cookin'?</h3>
