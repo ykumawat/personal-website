e2ccae5978d0463e191980fd66a61d603c6d6066
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-11-23 23:10:48 -0500

    blog links

--- src/PostList.js
+++ src/PostList.js
@@ -6,7 +6,7 @@ const PostList = (props) => {
   return (
     <div style={{border: "double", marginBottom: "20px"}}>
       <div>
-        <button className="btn btn-primary post">View Post >> </button>
+        <a href={`https://medium.com/@yamini011/${props.post.uniqueSlug}`} target="_blank" rel="noopener noreferrer"><button className="btn btn-primary post">View Post</button></a>
         <h2 style={{marginLeft:"10px", marginTop: "5px"}}>{props.post.title}</h2>
       </div>
       <p style={{paddingLeft: "10px"}}>{props.post.virtuals.subtitle}</p>
