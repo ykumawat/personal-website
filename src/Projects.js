113d8d81e0e8f8e6ff1b912a4fb22bfba4e6da51
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-11-25 15:46:33 -0500

    changed arrangement of project slides

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
-import Gist from 'react-gist'
+
+// <h4>Technologies and libraries used: </h4> <p>Ruby on Rails, JavaScript, React.JS, Redux, PostgreSQL, Google Cloud Vision API, Google Natural Language Processing API</p>
 
 class Projects extends React.Component {
 
@@ -10,8 +11,22 @@ class Projects extends React.Component {
         <Carousel>
           <Carousel.Item>
             <div className="slide-container">
-              <button className="project-demo-button-link">Demo</button>
-              <a href="https://github.com/ykumawat/ss_final_project" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
+              <div className="row">
+                <div className="col-4">
+                  <button className="project-demo-button-link">Demo</button>
+                </div>
+                <div className="col-4">
+                  <h2 style={{color: "#B68FA3", textAlign:"center"}}>S+S</h2>
+                </div>
+                <div className="col-4">
+                  <a href="https://github.com/ykumawat/ss_final_project" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12">
+                  <p style={{color: "#B68FA3", textAlign:"center"}}>Easily and efficiently manage contacts and notes with S+S, an image-to-text contact and document creator.</p>
+                </div>
+              </div>
               <div>
                 <div className="project-video">
                   <iframe width="560" height="315" src="https://www.youtube.com/embed/FP4Kir2us30" frameborder="0" gesture="media" allowFullScreen></iframe>
@@ -21,20 +36,34 @@ class Projects extends React.Component {
                 </div>
               </div>
             </div>
-          <Carousel.Caption style={{color: "#91736C", textShadow: "none", paddingBottom: "10px"}}>
-            <h3>S+S</h3>
-            <p>Easily and efficiently manage contacts and notes with S+S, an image-to-text contact and document creator.</p>
+          <Carousel.Caption style={{color: "#B68FA3", textShadow: "none", paddingBottom: "10px"}}>
+            <h4 style={{textDecoration: "underline"}}>Technologies and libraries used:</h4>
+            <p>Ruby on Rails, JavaScript, React.js, Redux, PostgreSQL, Google Cloud Vision API, Google Natural Language Processing API</p>
           </Carousel.Caption>
           </Carousel.Item>
 
           <Carousel.Item>
           <div className="slide-container">
-            <button className="project-demo-button-link">Demo</button>
-            <div className="dropdown">
-              <button className="project-code-link">Code</button>
-              <div className="dropdown-content">
-                <a href="https://github.com/ykumawat/nyc-rec-frontend" target="_blank" rel="noopener noreferrer">Frontend</a>
-                <a href="https://github.com/ykumawat/nyc-rec-backend" target="_blank" rel="noopener noreferrer">Backend</a>
+            <div className="row">
+              <div className="col-4">
+                <button className="project-demo-button-link">Demo</button>
+              </div>
+              <div className="col-4">
+                <h2 style={{color: "#B68FA3", textAlign:"center"}}>NYC Rec Ref</h2>
+              </div>
+              <div className="col-4">
+                <div className="dropdown">
+                  <button className="project-code-link">Code</button>
+                  <div className="dropdown-content">
+                    <a href="https://github.com/ykumawat/nyc-rec-frontend" target="_blank" rel="noopener noreferrer">Frontend</a>
+                    <a href="https://github.com/ykumawat/nyc-rec-backend" target="_blank" rel="noopener noreferrer">Backend</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <p style={{color: "#B68FA3", textAlign: "center"}}>Discover NYC recreational areas and favorite them on your in-app user profile for easy accessibility.</p>
               </div>
             </div>
             <div>
@@ -46,16 +75,30 @@ class Projects extends React.Component {
             </div>
             </div>
           </div>
-          <Carousel.Caption style={{color: "#91736C", textShadow: "none", paddingBottom: "10px"}}>
-            <h3>NYC Rec Ref</h3>
-            <p>Discover NYC recreational areas and favorite them on your in-app user profile for easy accessibility.</p>
+          <Carousel.Caption style={{color: "#B68FA3", textShadow: "none", paddingBottom: "10px"}}>
+            <h4 style={{textDecoration: "underline"}}>Technologies and libraries used:</h4>
+            <p>JavaScript, React.js, NYC Gov Data, Wunderground API, JWT, BCrypt</p>
           </Carousel.Caption>
           </Carousel.Item>
 
           <Carousel.Item>
           <div className="slide-container">
-            <button className="project-demo-button-link">Demo</button>
-            <a href="https://github.com/ykumawat/fake-stockmarket" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
+            <div className="row">
+              <div className="col-4">
+                <button className="project-demo-button-link">Demo</button>
+              </div>
+              <div className="col-4">
+                <h2 style={{color: "#B68FA3", textAlign:"center"}}>YaKush.ly</h2>
+              </div>
+              <div className="col-4">
+                <a href="https://github.com/ykumawat/fake-stockmarket" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <p style={{color: "#B68FA3", textAlign: "center"}}>Replicated a stock market and money sharing app using user created content.</p>
+              </div>
+            </div>
             <div>
               <div className="project-video">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/9-p6BxYvOcM" frameborder="0" gesture="media" allowFullScreen></iframe>
@@ -65,15 +108,29 @@ class Projects extends React.Component {
               </div>
             </div>
           </div>
-          <Carousel.Caption style={{color: "#91736C", textShadow: "none", paddingBottom: "10px"}}>
-            <h3>YaKush.ly</h3>
-            <p>Replicated a stock market and money sharing app using user created content.</p>
+          <Carousel.Caption style={{color: "#B68FA3", textShadow: "none", paddingBottom: "10px"}}>
+            <h4 style={{textDecoration: "underline"}}>Technologies and libraries used:</h4>
+            <p>Ruby on Rails, S&P 500 CSV data, JWT, BCrypt</p>
           </Carousel.Caption>
           </Carousel.Item>
 
           <Carousel.Item>
           <div className="slide-container">
-            <a href="https://github.com/ykumawat/module-one-final-project-guidelines-web-071717" target="_blank" rel="noopener noreferrer"><button className="project-code-link" style={{marginLeft: '900px'}}>Code</button></a>
+            <div className="row">
+              <div className="col-4">
+              </div>
+              <div className="col-4">
+                <h2 style={{color: "#B68FA3", textAlign:"center"}}>What's Cookin'?</h2>
+              </div>
+              <div className="col-4">
+                <a href="https://github.com/ykumawat/module-one-final-project-guidelines-web-071717" target="_blank" rel="noopener noreferrer"><button className="project-code-link" style={{marginLeft: '900px'}}>Code</button></a>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <p style={{color: "#B68FA3", textAlign: "center"}}>Quickly find the perfect recipe using a specific preference and ingredient.</p>
+              </div>
+            </div>
             <div>
               <div className="project-video">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/shbSK6FcWkM" frameborder="0" gesture="media" allowFullScreen></iframe>
@@ -83,9 +140,9 @@ class Projects extends React.Component {
               </div>
             </div>
           </div>
-          <Carousel.Caption style={{color: "#91736C", textShadow: "none", paddingBottom: "10px"}}>
-            <h3>What's Cookin'?</h3>
-            <p>Quickly find the perfect recipe using a specific preference and ingredient.</p>
+          <Carousel.Caption style={{color: "#B68FA3", textShadow: "none", paddingBottom: "10px"}}>
+            <h4 style={{textDecoration: "underline"}}>Technologies and libraries used:</h4>
+            <p>Ruby, Spoonacular JSON data</p>
           </Carousel.Caption>
           </Carousel.Item>
 
