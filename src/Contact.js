866394406a9846d315c1ac314b3bfa3a934b84d8
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-11-25 16:31:56 -0500

    embedded google contact form

--- src/Contact.js
+++ src/Contact.js
@@ -6,40 +6,8 @@ export default class Contact extends React.Component {
   constructor() {
     super()
     this.state = {
-      showModal: false,
-      type: '',
-      message: ''
+      showModal: false
     }
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault()
-    this.setState({
-      type: 'preparing to send',
-      message: 'Sending...'
-    }, this.sendMessage)
-  }
-
-  sendMessage() {
-    console.log("still working on this")
-  }
-
-  handleChangeName = (event) => {
-    this.setState({
-      name: event.target.value
-    })
-  }
-
-  handleChangeEmail = (event) => {
-    this.setState({
-      email: event.target.value
-    })
-  }
-
-  handleChangeMessage = (event) => {
-    this.setState({
-      message: event.target.value
-    })
   }
 
   open = (event) => {
@@ -61,37 +29,7 @@ export default class Contact extends React.Component {
           Contact Me!
         </button>
         <Modal show={this.state.showModal} onHide={this.close}>
-          <Modal.Header>
-          <Modal.Title>Message me for collaboration on projects or networking!
-          </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <form onSubmit={this.handleSubmit}>
-            <FormGroup controlId="formControlsText">
-              <ControlLabel>Name*</ControlLabel>
-              <FormControl type="text" label="Name" placeholder="Your Full Name" onChange={this.handleChangeName}/>
-            </FormGroup>
-
-            <FormGroup controlId="formControlsEmail">
-              <ControlLabel>Email*</ControlLabel>
-              <FormControl type="text" label="Email" placeholder="Your Email" onChange={this.handleChangeEmail}/>
-            </FormGroup>
-
-            <FormGroup controlId="formControlsTextarea">
-              <ControlLabel>Message*</ControlLabel>
-              <FormControl componentClass="textarea" placeholder="Your Message...(Funny gifs always welcome)" onChange={this.handleChangeMessage}/>
-            </FormGroup>
-
-            A copy of this message will be emailed to you. I always respond to messages within 48 hours!
-            <br/><br/>
-            <button className="btn btn-primary">Submit</button>
-            </form>
-          </Modal.Body>
-          <Modal.Footer>
-
-
-          </Modal.Footer>
-
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdn38-9HUJxStgzdNa4InP6giu7kvNV2eJ3PezMzWHMex5IEw/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
         </Modal>
       </div>
     )
