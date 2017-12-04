3450034ccc438d1f7e1064a951c17ca1a4b34e3f
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-12-03 19:17:38 -0500

    editing projects display page

--- src/Projects.js
+++ src/Projects.js
@@ -11,11 +11,14 @@ class Projects extends React.Component {
 
       <div style={{paddingBottom: "20px", borderBottom:"2px solid #B68FA3"}}>
         <div className="row">
-          <div className="col-12">
-            <h2 style={{color: "#B68FA3", textAlign:"center", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>S+S</h2>
-            <p style={{color: "#B68FA3", textAlign:"center", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Easily and efficiently manage contacts and notes with S+S, an image-to-text contact and document creator.</p>
+          <div className="col-11">
+            <h1 style={{color: "#B68FA3", textAlign:"left", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>S+S</h1>
+            <h4 style={{color: "#B68FA3", textAlign:"left", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Easily and efficiently manage contacts and notes with S+S, an image-to-text contact and document creator.</h4>
+          </div>
+          <div className="col-1">
             <a href="https://github.com/ykumawat/ss_final_project" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
           </div>
+        </div>
           <div className="row">
             <div className="col-6">
               <iframe className="project-video" src="https://www.youtube.com/embed/FP4Kir2us30" frameborder="0" gesture="media" allowFullScreen></iframe>
@@ -28,22 +31,22 @@ class Projects extends React.Component {
             <h4 style={{textDecoration: "underline", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif", textAlign:"center"}}>Technologies and libraries used:</h4>
             <p style={{textAlign:"center"}}>Ruby on Rails, JavaScript, React.js, Redux, PostgreSQL, Google Cloud Vision API, Google Natural Language Processing API</p>
           </div>
-        </div>
       </div>
 
       <div style={{paddingBottom: "20px", borderBottom:"2px solid #B68FA3"}}>
         <div className="row">
-          <div className="col-12">
-            <h2 style={{color: "#B68FA3", textAlign:"center", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Nyc RecRef</h2>
-            <p style={{color: "#B68FA3", textAlign: "center", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Discover NYC recreational areas and favorite them on your in-app user profile for easy accessibility.</p>
-          <div className="dropdown">
+          <div className="col-11">
+            <h1 style={{color: "#B68FA3", textAlign:"left", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Nyc RecRef</h1>
+            <h4 style={{color: "#B68FA3", textAlign: "left", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Discover NYC recreational areas and favorite them on your in-app user profile for easy accessibility.</h4>
+          </div>
+
+          <div className="dropdown col-1">
             <button className="project-code-link">Code</button>
             <div className="dropdown-content">
               <a href="https://github.com/ykumawat/nyc-rec-frontend" target="_blank" rel="noopener noreferrer">Frontend</a>
               <a href="https://github.com/ykumawat/nyc-rec-backend" target="_blank" rel="noopener noreferrer">Backend</a>
             </div>
           </div>
-        </div>
         </div>
         <div className="row">
           <div className="col-6">
@@ -53,17 +56,19 @@ class Projects extends React.Component {
             <iframe className="project-code-snippet" src="https://repl.it/@yamini011/NYCRecRefSnippet?lite=true"></iframe>
           </div>
         </div>
-            <div className="col-12">
-              <h4 style={{textDecoration: "underline", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif", textAlign:"center"}}>Technologies and libraries used:</h4>
-              <p style={{textAlign:"center"}}>JavaScript, React.js, NYC Gov Data, Wunderground API, JWT, BCrypt</p>
+          <div className="col-12">
+            <h4 style={{textDecoration: "underline", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif", textAlign:"center"}}>Technologies and libraries used:</h4>
+            <p style={{textAlign:"center"}}>JavaScript, React.js, NYC Gov Data, Wunderground API, JWT, BCrypt</p>
           </div>
         </div>
 
         <div style={{paddingBottom: "20px", borderBottom:"2px solid #B68FA3"}}>
           <div className="row">
-            <div className="col-12">
-              <h2 style={{color: "#B68FA3", textAlign:"center", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>YaKush.ly</h2>
-              <p style={{color: "#B68FA3", textAlign: "center", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Replicated a stock market and money sharing app using user created content.</p>
+            <div className="col-11">
+              <h1 style={{color: "#B68FA3", textAlign:"left", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>YaKush.ly</h1>
+              <h4 style={{color: "#B68FA3", textAlign: "left", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Replicated a stock market and money sharing app using user created content.</h4>
+            </div>
+            <div className="col-1">
               <a href="https://github.com/ykumawat/fake-stockmarket" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
             </div>
           </div>
@@ -83,9 +88,11 @@ class Projects extends React.Component {
 
         <div style={{paddingBottom: "20px", borderBottom:"2px solid #B68FA3"}}>
           <div className="row">
-            <div className="col-12">
-              <h2 style={{color: "#B68FA3", textAlign:"center", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>What's Cookin'</h2>
-              <p style={{color: "#B68FA3", textAlign: "center", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Quickly find the perfect recipe using a specific preference and ingredient.</p>
+            <div className="col-11">
+              <h1 style={{color: "#B68FA3", textAlign:"left", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>What's Cookin'</h1>
+              <h4 style={{color: "#B68FA3", textAlign: "left", fontFamily:"Futura, Trebuchet MS, Arial, sans-serif"}}>Quickly find the perfect recipe using a specific preference and ingredient.</h4>
+            </div>
+            <div className="col-1">
               <a href="https://github.com/ykumawat/module-one-final-project-guidelines-web-071717" target="_blank" rel="noopener noreferrer"><button className="project-code-link">Code</button></a>
             </div>
           </div>
